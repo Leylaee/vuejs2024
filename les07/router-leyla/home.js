85651c564edf30b68657ceaea43930dfc714d72b
Leylaee <ferhan.96@hotmail.com>
2024-04-17 18:46:49 +0200

    oef

---
+++ les07/router-leyla/home.js
@@ -0,0 +1,78 @@
+const home = {
+    template: '#homeTemplate',
+    data: function () {
+        return {
+            items: [
+                {
+                id:1, 
+                naam: 'hondenpak superman',
+                omschrijving: 'Neem je hond mee als sidekick op al je avonturen!',
+                prijs: 29.99, 
+                afbeeldingSrc: 'elias-castillo-7-ToFEHzMNw-unsplash.jpg',
+                besteld: 0
+                },
+                {
+                id:2, 
+                naam: 'Wonder Woman', 
+                omschrijving: 'Laat de kleine meid tonen hoe speciaal ze is!',
+                prijs: 34.99,
+                afbeeldingSrc: 'gabriela-braga-CVEWeDNsJP4-unsplash.jpg',
+                besteld: 0
+                },
+                {
+                id:3, 
+                naam: 'Spiderman masker',
+                omschrijving: 'Je wil niet altijd dat je herkent wordt voor je goede daden!',
+                prijs: 14.99, 
+                afbeeldingSrc: 'joey-nicotra-0EI_4R2r0qg-unsplash.jpg',
+                besteld: 0
+                }
+            ],
+            winkelwagen : [],
+            pathSmall:'images/small/',
+            pathBig:'images/big/',
+            
+           
+        }
+    },
+    methods : {
+        toonGroteFoto: function(path){
+            window.open(path);
+        },
+        VoegToeAanWinkelwagen: function(index) {
+            this.items[index].besteld++;
+            this.$root.aantalInWinkelwagen++;
+            this.saveLocalStorageWinkelwagen();
+            this.toonWinkelwagenMelding();
+        },
+        toonWinkelwagenMelding : function() {
+            this.$root.showWinkelwagen = false;
+            setTimeout(function(scope) {
+                scope.$root.showWinkelwagen = true;
+            }, 100,this);
+        },
+        saveLocalStorageWinkelwagen: function() {
+            let temp = this.items.filter((item) => item.besteld !== 0);
+            localStorage.setItem('winkelwagen', JSON.stringify(temp));
+            },
+       
+       
+        
+    },
+    mounted() {
+        if (localStorage.getItem("winkelwagen")) {
+            let tempLs = JSON.parse(localStorage.getItem("winkelwagen"));
+            //alle items in LS overlopen
+            //find in de data overeenkomstig item:
+            //update item in data met besteld uit item van LS
+            //update aantalInWinkelwagen
+            tempLs.forEach((itemInLs) => {
+            let gevondenItemInData = this.items.find(itemInData => itemInData.id == itemInLs.id);
+            if (gevondenItemInData) {
+            gevondenItemInData.besteld = itemInLs.besteld
+            this.aantalInWinkelwagen += (itemInLs.besteld)
+            }
+             })		
+        }
+    }
+}
---
+++ les07/router-leyla/winkelwagen.js
@@ -0,0 +1,67 @@
+const winkelwagen = {
+    template: '#winkelwagenTemplate',
+    data: function () {
+        return {
+            items: [],
+            pathSmall:'images/small/',
+            pathBig:'images/big/',
+         
+           
+        }
+    },
+    methods : {
+        toonGroteFoto: function(path){
+            window.open(path);
+        },
+        verwijder: function(index) {
+            this.items = this.items.filter((item,indx) => indx !== index)
+            this.saveLocalStorageWinkelwagen();
+        } ,
+        aantalAanpassing: function(index) {
+            if(this.items[index].besteld == 0){
+                this.items.splice(index,1);
+            }
+            this.saveLocalStorageWinkelwagen();
+        },
+        saveLocalStorageWinkelwagen: function() {
+            localStorage.setItem('winkelwagen', JSON.stringify(this.items));
+        }
+       
+    },
+    computed: {
+        totalePrijs: function () {
+            let totaal = 0;
+            for(let i = 0; i < this.items.length; i++){
+                if(this.items[i].besteld > 0){
+                    totaal += this.items[i].besteld * this.items[i].prijs
+                } 
+            }
+            return totaal.toFixed(2);
+        },
+        totaalAantalItems: function () {
+            let totaal = 0;
+            if(this.items.length > 0 ){
+                for(let i = 0; i < this.items.length; i++){
+                if(this.items[i].besteld > 0){
+                    totaal += parseInt(this.items[i].besteld);
+                } 
+            }
+        
+
+            }
+            return totaal;
+        
+        }
+    },
+    watch: {
+        totaalAantalItems(newValue) {
+        this.$root.aantalInWinkelwagen = newValue
+       }
+     },
+    mounted()  {
+        if (localStorage.getItem("winkelwagen")) {
+            this.items = JSON.parse(localStorage.getItem("winkelwagen"))
+           }
+    }
+
+}
